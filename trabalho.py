1e2a55c43ff52c7cb38a77136ea9250ef0f9390a
crishard <crislantorres9@gmail.com>
2023-06-08 11:02:50 -0300

    arquivo base do trabalho

---
+++ trabalho.py
@@ -0,0 +1,12 @@
+import json
+import sys
+
+class GraphTool:
+    def __init__(self):
+        self.graphs = []
+
+def main():
+    graph_tool = GraphTool()
+
+if __name__ == '__main__':
+    main()
