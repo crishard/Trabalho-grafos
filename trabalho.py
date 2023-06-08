7281c2efd9d4f2859ab71a78cb12d840aae434e0
crishard <crislantorres9@gmail.com>
2023-06-08 11:20:05 -0300

    criando função para verificar quais vertices não são alcançaveis a partir de um vértice de partida

--- trabalho.py
+++ trabalho.py
@@ -113,6 +113,28 @@ class GraphTool:
                 return
         print("Graph not found:", graph_id)
 
+    # função com bug
+    def find_unreachable_vertices(self, graph_id, starting_vertex):
+        for graph in self.graphs:
+            if graph['id'] == graph_id:
+                vertices = graph['vertices']
+                edges = graph['edges']
+                reachable_vertices = [starting_vertex]
+                queue = [starting_vertex]
+                while queue:
+                    current_vertex = queue.pop(0)
+                    for edge in edges:
+                        if edge[0] == current_vertex and edge[1] not in reachable_vertices:
+                            reachable_vertices.append(edge[1])
+                            queue.append(edge[1])
+                        elif edge[1] == current_vertex and edge[0] not in reachable_vertices:
+                            reachable_vertices.append(edge[0])
+                            queue.append(edge[0])
+                unreachable_vertices = [vertex for vertex in vertices if vertex not in reachable_vertices]
+                print("Unreachable vertices from", starting_vertex, "in graph", graph_id, "are:", unreachable_vertices)
+                return
+        print("Graph not found:", graph_id)
+
 
     # função auxiliar
     def is_connected(self, vertices, edges):
