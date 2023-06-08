39df86a2348601cfc3d951a7220845716cded302
crishard <crislantorres9@gmail.com>
2023-06-08 11:05:11 -0300

    Função para receber os comandos no tetminal

--- trabalho.py
+++ trabalho.py
@@ -14,9 +14,23 @@ class GraphTool:
         except FileNotFoundError:
             print("File not found:", filename)
 
+    def run_command(self, command):
+        if command[0] == 'grafos':
+            if len(command) == 3 and command[1] == 'carregar':
+                self.load_graphs_from_file(command[2])
+            elif command[1] == 'sair':
+                sys.exit()
+            else:
+                print("Invalid command")
+        else:
+            print("Invalid command")
 
 def main():
     graph_tool = GraphTool()
+    while True:
+        user_input = input("Enter a command: ")
+        command = user_input.split()
+        graph_tool.run_command(command)
 
 if __name__ == '__main__':
     main()
