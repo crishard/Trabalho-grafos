3a8b0212cb509a425414dc19ae66b0f4e62a68dd
crishard <crislantorres9@gmail.com>
2023-06-08 11:03:35 -0300

    Função para carregar arquivo json

--- trabalho.py
+++ trabalho.py
@@ -5,6 +5,16 @@ class GraphTool:
     def __init__(self):
         self.graphs = []
 
+    def load_graphs_from_file(self, filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                self.graphs = data['graphs']
+                print("Graphs loaded successfully from", filename)
+        except FileNotFoundError:
+            print("File not found:", filename)
+
+
 def main():
     graph_tool = GraphTool()
 
