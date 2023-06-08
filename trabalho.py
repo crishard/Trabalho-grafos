5a3c5ee2054602d9d0ff41249684f3355ffae7eb
crishard <crislantorres9@gmail.com>
2023-06-08 11:14:50 -0300

    criando função para verificar os graus de cada vértice de determinado grafo

--- trabalho.py
+++ trabalho.py
@@ -63,7 +63,22 @@ class GraphTool:
         else:
             print("No complete graphs found")
 
+    def get_vertex_degrees(self, graph_id):
+        for graph in self.graphs:
+            if graph['id'] == graph_id:
+                vertices = graph['vertices']
+                edges = graph['edges']
+                degrees = {vertex: 0 for vertex in vertices}
+                for edge in edges:
+                    degrees[edge[0]] += 1
+                    degrees[edge[1]] += 1
+                print("Vertex degrees for graph", graph_id)
+                for vertex, degree in degrees.items():
+                    print("Vertex:", vertex, "Degree:", degree)
+                return
+        print("Graph not found:", graph_id)
 
+    # função auxiliar
     def is_connected(self, vertices, edges):
         if not vertices:
             return False
@@ -81,6 +96,7 @@ class GraphTool:
                     queue.append(edge[0])
         return len(reachable_vertices) == len(vertices)
 
+    # função auxiliar
     def is_complete(self, vertices, edges):
         if not vertices:
             return False
