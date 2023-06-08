e7271b8a1aca72d05da58e978b41183c2116c8d9
crishard <crislantorres9@gmail.com>
2023-06-08 11:07:52 -0300

    criando função para verificar a presença de pseudografos

--- trabalho.py
+++ trabalho.py
@@ -24,6 +24,20 @@ class GraphTool:
             print("Multigraphs found id's:", multigraphs)
         else:
             print("No multigraphs found")
+    
+    def check_pseudographs(self):
+        pseudographs = []
+        for graph in self.graphs:
+            vertices = graph['vertices']
+            edges = graph['edges']
+            for edge in edges:
+                if edge[0] == edge[1] and edge[0] in vertices:
+                    pseudographs.append(graph['id'])
+                    break
+        if pseudographs:
+            print("Pseudographs found:", pseudographs)
+        else:
+            print("No pseudographs found")
 
     def run_command(self, command):
         if command[0] == 'grafos':
