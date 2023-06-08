ae0206fce3f44f25d605fbdd6f38bc22d5957059
crishard <crislantorres9@gmail.com>
2023-06-08 11:29:58 -0300

    Retirando comentário

--- functions/count_connected_vertices.py
+++ functions/count_connected_vertices.py
@@ -1,20 +1,20 @@
-# def count_connected_vertices(self, graph_id, start_vertex):
-    #     for graph in self.graphs:
-    #         if graph['id'] == graph_id:
-    #             vertices = graph['vertices']
-    #             edges = graph['edges']
-    #             queue = [start_vertex]
-    #             visited = set()
-    #             while queue:
-    #                 current_vertex = queue.pop(0)
-    #                 if current_vertex in visited:
-    #                     continue
-    #                 visited.add(current_vertex)
-    #                 for edge in edges:
-    #                     if edge[0] == current_vertex and edge[1] not in visited:
-    #                         queue.append(edge[1])
-    #                     elif edge[1] == current_vertex and edge[0] not in visited:
-    #                         queue.append(edge[0])
-    #             print("Number of connected vertices from", start_vertex, "in graph", graph_id, "is:", len(visited))
-    #             return
-    #     print("Graph not found:", graph_id)
+def count_connected_vertices(self, graph_id, start_vertex):
+        for graph in self.graphs:
+            if graph['id'] == graph_id:
+                vertices = graph['vertices']
+                edges = graph['edges']
+                queue = [start_vertex]
+                visited = set()
+                while queue:
+                    current_vertex = queue.pop(0)
+                    if current_vertex in visited:
+                        continue
+                    visited.add(current_vertex)
+                    for edge in edges:
+                        if edge[0] == current_vertex and edge[1] not in visited:
+                            queue.append(edge[1])
+                        elif edge[1] == current_vertex and edge[0] not in visited:
+                            queue.append(edge[0])
+                print("Number of connected vertices from", start_vertex, "in graph", graph_id, "is:", len(visited))
+                return
+        print("Graph not found:", graph_id)
